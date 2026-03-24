59f49ea90e3d19d307ec01c0ca79cbc508525ca1
menualvhp-create <menualvhp@gmail.com>
2026-03-24 13:30:10 +0530

    new images added

--- src/data/images.ts
+++ src/data/images.ts
@@ -4,43 +4,43 @@ export const IMAGES = {
     alt: "Luxury Ella Landscape"
   },
   nineArch: {
-    src: "https://www.orienthotelsl.com/wp-content/uploads/2023/01/Nine-Arch-Bridge-Ella-1200x630-1.jpg",
+    src: "/images/nineArch.jpg",
     alt: "Nine Arch Bridge Ella"
   },
   littleAdams: {
-    src: "https://nerdnomads.com/wp-content/uploads/2014/04/DSC5132.jpg",
+    src: "/images/littleAdams.jpg",
     alt: "Little Adam's Peak Sunrise"
   },
   ellaRock: {
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAswTDhvZxKzNoZt2GR9pNdkYCB-IEADP0lQ&s",
+    src: "/images/ellaRock.jpg",
     alt: "Ella Rock Hiking Trail"
   },
   demodaraLoop: {
-    src: "https://cdn.getyourguide.com/image/format=auto,fit=crop,gravity=center,quality=60,width=450,height=450,dpr=2/tour_img/70c149ee2cf0929a80c63b09e765f2bfee4cdf50ef273bb8b333096a3e9902ac.jpeg",
+    src: "/images/demodaraLoop.jpg",
     alt: "Demodara Nine Arch Railway Loop"
   },
   kudaRavana: {
-    src: "https://place.com-photos.com/241/kuda-ravana-ella-water-fall-AF1QipNxuk_x1HxL_tjQz7dAkQx1smoaKUEQf3WcSt6s.jpg",
+    src: "/images/kudaRavana.jpg",
     alt: "Kuda Ravana Ella Waterfall"
   },
   diyaluma: {
-    src: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/12/6e/f0/29.jpg",
+    src: "/images/diyaluma.jpg",
     alt: "Diyaluma Falls Sri Lanka"
   },
   blueWaterPond: {
-    src: "https://lakpura.com/cdn/shop/products/LK83090100-02-E-1280-720.jpg?v=1624356635&width=1445",
+    src: "/images/blueWaterPond.jpg",
     alt: "Blue Water Pond"
   },
   bakersBend: {
-    src: "https://amazinglanka.com/wp/wp-content/uploads/2019/03/bakersbend-4.jpg",
+    src: "/images/bakersBend.jpg",
     alt: "Baker's Bend Ella Viewpoint"
   },
   liptonsSeat: {
-    src: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2f/fe/ba/38/caption.jpg?w=800&h=800&s=1",
+    src: "/images/liptonsSeat.jpg",
     alt: "Lipton's Seat Tea Plantation Viewpoint"
   },
   ravanaFalls: {
-    src: "https://media-cdn.tripadvisor.com/media/photo-o/0f/c0/83/8f/ravana-ella-falls.jpg",
+    src: "/images/ravanaFalls.jpg",
     alt: "Ravana Falls Ella"
   },
   aboutGuide: {
